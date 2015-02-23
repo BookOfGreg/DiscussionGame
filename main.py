f4b9817084a9f0371d18c82a3fa04bc744dd04df
Greg Myers <neonmd@hotmail.co.uk>
2015-02-23 11:34:46 +0000

    Did things

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-def adder(self,first,second):
-  return (first + second)
+class Proponent:
+  def has_to_be(self, argument, game):
+    return False
+
+# class Opponent:
+#   def could_be(self, argument, game):
+#     return False
+
+#   def concede(self, argument, game):
+#     return False
+
+#   def retract(self, argument, game):
+#     return False
+
+class Argument:
+  pass
+  # label = "string"
+  # attackers = set(arguments...)
+
+class Game:
+  def __init__(self, arguments, attack_relations):
+    self.arguments = arguments
+    self.attack_relations = attack_relations
 
 if __name__ == "__main__": main()
--- main_test.py
+++ main_test.py
@@ -1,6 +1,24 @@
-import main
+from main import *
 import unittest
 
-class MyTest(unittest.TestCase):
-  def test_adder(self):
-    self.assertEqual(main.adder(self,2,3),6)
+class DiscussionTest(unittest.TestCase):
+  def test_has_to_be(self):
+    game = Game({},{})
+    argument = Argument()
+    expected_game = Game({argument}, {})
+    proponent = Proponent()
+    self.assertEqual(self, proponent.has_to_be(argument, game), expected_game )
+
+
+
+
+
+
+
+  # def setUp(self):
+    # self.proponent = Proponent()
+    # self.opponent = "opponent"
+    # self.argument = "a"
+
+  # def test_has_to_be(self):
+    # self.assertTrue(self.proponent.has_to_be(self.argument))
