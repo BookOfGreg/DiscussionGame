b3227962299d91677e0c84a770e177e011fe4235
Greg Myers <neonmd@hotmail.co.uk>
2015-02-23 16:51:55 +0000

    did things

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ class Opponent:
   def could_be(self, argument, game):
     return game.add(argument)
 
-#   def concede(self, argument, game):
-#     return False
+  def concede(self, argument, game):
+    return False
 
 #   def retract(self, argument, game):
 #     return False
@@ -18,13 +18,16 @@ class Argument:
   # attackers = set(arguments...)
 
 class Game:
-  def __init__(self, arguments, attack_relations):
-    self.arguments = arguments
+  def __init__(self, arguments=None, attack_relations=None):
+    if arguments is None: arguments = set()
+    if attack_relations is None: attack_relations = list()
+
     self.attack_relations = attack_relations
+    self.arguments = arguments
     self.last_argument = None
 
   def add(self, argument):
-    if self.last_argument != None: self.attack_relations.append((argument, self.last_argument))
+    if self.last_argument is not None: self.attack_relations.append((argument, self.last_argument))
     self.arguments.add(argument)
     self.last_argument = argument
     return self
--- main_test.py
+++ main_test.py
@@ -3,22 +3,29 @@ import unittest
 
 class DiscussionTest(unittest.TestCase):
   def test_has_to_be(self):
-    expected_game = Game(set([self.argument]), list())
+    expected_game = Game(set([self.argument]))
 
-    self.assertEqual(self.step_game.arguments, expected_game.arguments )
+    self.assertEqual(self.game.arguments, expected_game.arguments)
 
   def test_could_be(self):
-    counter_argument = Argument()
-
-    expected_game = Game(set([self.argument, counter_argument]), list([(counter_argument, self.argument)]) )
-    new_game = self.opponent.could_be(counter_argument, self.step_game)
+    second_argument = Argument()
+    expected_game = Game(set([self.argument, second_argument]),
+                         list([(second_argument, self.argument)]))
+    new_game = self.opponent.could_be(second_argument, self.game)
 
     self.assertEqual(new_game.arguments, expected_game.arguments)
     self.assertEqual(new_game.attack_relations, expected_game.attack_relations)
 
+  def test_concede(self):
+    second_argument = Argument()
+    third_argument = Argument()
+    game_state = Game(set([self.argument, second_argument, third_argument]),
+                      list([(second_argument, self.argument),
+                            (third_argument, second_argument)]))
+    new_game = self.opponent.concede(game_state, self.game)
+
   def setUp(self):
-    self.game = Game(set(),list())
     self.proponent = Proponent()
     self.opponent = Opponent()
     self.argument = Argument()
-    self.step_game = self.proponent.has_to_be(self.argument, self.game)
+    self.game = self.proponent.has_to_be(self.argument, Game())
