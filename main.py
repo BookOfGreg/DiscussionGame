6afa26870eb59fd31ec2ce676c83d52a7aadfe29
Greg Myers <neonmd@hotmail.co.uk>
2015-02-23 16:05:42 +0000

    Got could_be

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class Proponent:
 
 class Opponent:
   def could_be(self, argument, game):
-    return game.add(argument, game.arguments.last())
+    return game.add(argument, (argument, game.last_argument))
 
 #   def concede(self, argument, game):
 #     return False
@@ -21,10 +21,12 @@ class Game:
   def __init__(self, arguments, attack_relations):
     self.arguments = arguments
     self.attack_relations = attack_relations
+    self.last_argument = None
 
-  def add(self, argument, attacks=None):
-    if attacks != None: self.attack_relations.add((argument, attacks))
+  def add(self, argument, attack_relation=None):
+    if attack_relation != None: self.attack_relations.append(attack_relation)
     self.arguments.add(argument)
+    self.last_argument = argument
     return self
 
 if __name__ == "__main__": main()
--- main_test.py
+++ main_test.py
@@ -3,23 +3,22 @@ import unittest
 
 class DiscussionTest(unittest.TestCase):
   def test_has_to_be(self):
-    game = Game(OrderedSet(),OrderedSet())
-    expected_game = Game(OrderedSet([self.argument]), OrderedSet())
+    expected_game = Game(set([self.argument]), list())
 
-    self.assertEqual(self.proponent.has_to_be(self.argument, game).arguments, expected_game.arguments )
+    self.assertEqual(self.step_game.arguments, expected_game.arguments )
 
   def test_could_be(self):
     counter_argument = Argument()
 
-    game = Game(OrderedSet([self.argument]), OrderedSet())
-    expected_game = Game(OrderedSet([self.argument, counter_argument]), OrderedSet([(counter_argument, self.argument)]) )
+    expected_game = Game(set([self.argument, counter_argument]), list([(counter_argument, self.argument)]) )
+    new_game = self.opponent.could_be(counter_argument, self.step_game)
 
-    new_game = self.opponent.could_be(self.argument, game)
     self.assertEqual(new_game.arguments, expected_game.arguments)
     self.assertEqual(new_game.attack_relations, expected_game.attack_relations)
 
-
   def setUp(self):
+    self.game = Game(set(),list())
     self.proponent = Proponent()
     self.opponent = Opponent()
     self.argument = Argument()
+    self.step_game = self.proponent.has_to_be(self.argument, self.game)
