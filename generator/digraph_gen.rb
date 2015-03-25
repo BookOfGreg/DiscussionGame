1cb09008124bf2b5891cc57c0d5b9515388d3b8e
Greg Myers <neonmd@hotmail.co.uk>
2015-03-25 18:45:13 +0000

    Ruby graph gen wip

---
+++ generator/digraph_gen.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env ruby
+
+class Array
+  def dequeue
+    self.shift
+  end
+
+  def enqueue obj
+    self.push obj
+  end
+
+  def tail
+    self[1..-1]
+  end
+end
+
+class DiGraph
+  def initialize nodes
+    @nodes = nodes
+    @attacks = []
+  end
+
+  def add_attack attacker, target
+    @attacks.enqueue [attacker, target]
+  end
+
+  def save_as filename
+    File.open(filename, "w") do |f|
+      f.puts @nodes.join " "
+      @attacks.each do |att|
+        f.puts att.join " "
+      end
+    end
+  end
+end
+
+def generate_node_names node_count
+  names = []
+  (0..node_count).each do |i|
+    names.enqueue i.to_s
+  end
+  return names
+end
+
+def balanced_tree_builder names
+  graph = DiGraph.new(names)
+  next_root = [names.first]
+  _, *unused_leaves = names.clone
+
+  while unused_leaves.any?
+    target = next_root.dequeue
+    leaf = unused_leaves.dequeue
+    graph.add_attack leaf, target
+  end
+  return graph
+end
+
+puts "use: ruby digraph_gen.rb nodes type"
+puts "type: one of x y z"
+desired_node_count = ARGV[0].to_i
+branches_count = ARGV[1].to_i
+
+names = generate_node_names desired_node_count
+graph = balanced_tree_builder names
+graph.save_as "balanced_tree"
