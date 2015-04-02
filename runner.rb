f5d9047104888ba814e2a0d71035c137c964d563
greg <greg@debian>
2015-04-02 22:41:51 +0100

    Avoiding debian problems

--- runner.rb
+++ runner.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
 
-# Dir.foreach("./graphs") do |file_name|
-["balanced_tree_9999",
- "balanced_tree_19999",
- "balanced_tree_29999",
- "balanced_tree_39999",
- "balanced_tree_49999",
- "balanced_tree_59999",
- "balanced_tree_69999",
- "balanced_tree_79999",
- "balanced_tree_89999",
- "balanced_tree_99999"].each do |file_name|
+Dir.foreach("./graphs") do |file_name|
+#["balanced_tree_9999",
+# "balanced_tree_19999",
+# "balanced_tree_29999",
+# "balanced_tree_39999",
+# "balanced_tree_49999",
+# "balanced_tree_59999",
+# "balanced_tree_69999",
+# "balanced_tree_79999",
+# "balanced_tree_89999",
+# "balanced_tree_99999"].each do |file_name|
   puts file_name
   next if file_name == '.' or file_name == '..'
   30.times do |num|
